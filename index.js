269f4d9a06621a89b643412238462e030c6c52f8
amolin09 <alex.molina5829@gmail.com>
2023-05-29 02:02:14 -0500

    finished all routes

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-let express = require('express')
+let express = require("express")
 
 let app = express()
 
@@ -13,6 +13,18 @@ app.get('/add', function(req, res){
 
   let sum = intNum1 + intNum2
   res.send(`The sum of ${num1} and ${num2} is ${sum}.`)
+})
+
+app.post('/multiply', function(req, res){
+  let num1 = req.body.num1
+  let num2 = req.body.num2
+
+  let intNum1 = Number(num1)
+  let intNum2 = Number(num2)
+
+  let product = intNum1 * intNum2
+  
+  res.send(`The product of ${intNum1} and ${intNum2} is ${product}.`)
 })
 
 app.get('/negate/:num', function(req, res){
